418c8ad53338f29748d52413a1c123f53606be1e
Mangalore Rakesh Shenoy <mangalos@usc.edu>
2016-07-11 03:45:14 -0700

    Update CSCI567_hw4_spring16.m

--- CSCI567_hw4_spring16.m
+++ CSCI567_hw4_spring16.m
@@ -1,26 +1,26 @@
-% blob = load('hw4_blob.mat');
-% circle = load('hw4_circle.mat');
-% K = [2, 3, 5];
-% 
-% for i=1:numel(K)
-%     k = K(i);
-%     clusters = kmeans(blob.data2,k);
-%     scatter(blob.data2(:,1), blob.data2(:,2), [], clusters, 'filled');
-%     title(sprintf('K-Means algorithm on Blob dataset with K=%d', k));
-%     xlabel('x1');
-%     ylabel('x2');
-%     print(sprintf('blob-%d',k), '-dpng');
-% end
-% 
-% for i=1:numel(K)
-%     k = K(i);
-%     clusters = kmeans(circle.points,k);
-%     scatter(circle.points(:,1), circle.points(:,2), [], clusters, 'filled');
-%     title(sprintf('K-Means algorithm on Circle dataset with K=%d', k));
-%     xlabel('x1');
-%     ylabel('x2');
-%     print(sprintf('circle-%d',k), '-dpng');
-% end
+ blob = load('hw4_blob.mat');
+ circle = load('hw4_circle.mat');
+ K = [2, 3, 5];
+ 
+ for i=1:numel(K)
+     k = K(i);
+     clusters = kmeans(blob.data2,k);
+     scatter(blob.data2(:,1), blob.data2(:,2), [], clusters, 'filled');
+     title(sprintf('K-Means algorithm on Blob dataset with K=%d', k));
+     xlabel('x1');
+     ylabel('x2');
+     print(sprintf('blob-%d',k), '-dpng');
+ end
+ 
+ for i=1:numel(K)
+     k = K(i);
+     clusters = kmeans(circle.points,k);
+     scatter(circle.points(:,1), circle.points(:,2), [], clusters, 'filled');
+     title(sprintf('K-Means algorithm on Circle dataset with K=%d', k));
+     xlabel('x1');
+     ylabel('x2');
+     print(sprintf('circle-%d',k), '-dpng');
+ end
 
 hw4img = imread('hw4.jpg');
 for i = [24]
